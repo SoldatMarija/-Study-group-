f0a9af87861cfebef70f2ce1771ae2be8d2f1217
Marija Soldat <soldat@work.co>
2024-05-28 12:19:28 +0200

    Add Hello World

---
+++ Challenge/HelloMarija.js
@@ -0,0 +1,20 @@
+
+function helloWorld(name) {
+    for (let i = 0; i<5; i++){
+       
+      console.log('Hello ' + name)
+           
+
+    }
+}
+     helloWorld('Marija')
+  
+   
+    
+   
+
+
+
+
+    
+    
